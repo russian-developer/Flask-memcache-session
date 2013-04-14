94d1fab6e78487b30b20edb20e8042298f748d1f
Hadya Kozlova <c.slednev@gmail.com>
2013-04-15 01:14:16 +0400

    setup fix

--- setup.py
+++ setup.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python
-from os.path import isfile, join
-import glob
-import os
-import re
-
 from setuptools import setup
 
-
-if isfile("MANIFEST"):
-    os.unlink("MANIFEST")
-
-
-TOPDIR = os.path.dirname(__file__) or "."
-
-
 setup(name="flask-memcache-session",
-      version = 1,
+      version = '1.0',
       description = "Use memcache for store session data",
       author = "Constantine Slednev",
       author_email = "c.slednev@gmail.com",
       url = "https://github.com/unk2k/Flask-memcache-session",
       license = "BSD License",
-      package_data={"": ["*.tar.gz"]},
-      include_package_data=True,
-      zip_safe=False,
+      py_modules = ['flask_memcache_session']
       )
